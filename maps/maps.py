5534f14fd400ebeb8a74d36251f9e6927aa2e461
Stanislav_Patratiy <blazka2011@yandex.ru>
2024-04-29 20:51:16 +0300

    add functions rating, chars_count in maps/maps.py

--- maps/maps.py
+++ maps/maps.py
@@ -4,28 +4,33 @@ from typing import Union
 class MapExercise:
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать средний рейтинг фильмов (rating_kinopoisk) у которых две или больше стран.
-        Фильмы у которых рейтинг не задан или равен 0 не учитывать в расчете среднего.
+        valid_movies = filter(
+            lambda movie: 'country' in movie and movie.get('rating_kinopoisk') and float(
+                movie['rating_kinopoisk']) > 0 and len(movie['country'].split(',')) >= 2,
+            list_of_movies
+        )
 
-        :param list_of_movies: Список фильмов.
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :return: Средний рейтинг фильмов у которых две или больше стран
-        """
-        pass
+        ratings = list(map(lambda movie: float(movie['rating_kinopoisk']), valid_movies))
+
+        if ratings:
+            return sum(ratings) / len(ratings)
+
+        return 0.0
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать количество букв 'и' в названиях всех фильмов с рейтингом (rating_kinopoisk) больше
-        или равным заданному значению
+        valid_movies = []
 
-        :param list_of_movies: Список фильмов
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :param rating: Заданный рейтинг
-        :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
-        или равным заданному значению
-        """
-        pass
+        for el in list_of_movies:
+            if 'rating_kinopoisk' in el and el['rating_kinopoisk']:
+                try:
+                    rating_kinopoisk = float(el['rating_kinopoisk'])
+                    if rating_kinopoisk >= rating:
+                        valid_movies.append(el)
+                except ValueError:
+                    # Обрабатываем случай, когда преобразование не удаётся
+                    continue
+
+        return sum(name.count('и') for name in map(lambda x: x['name'], valid_movies))
+
+
