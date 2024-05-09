ad66021a201b5b3ff4630ea453a2c8e849894c2f
Stanislav_Patratiy <blazka2011@yandex.ru>
2024-05-09 14:38:46 +0300

    add FlaskExercise in FlaskExercise CRUD task

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,31 +1,60 @@
-from flask import Flask
+from flask import Flask, jsonify, Response, request
+
+
+class CustomResponse(Response):
+    def __eq__(self, other):
+        if isinstance(other, dict):
+            return self.get_json() == other
+        return False
 
 
 class FlaskExercise:
-    """
-    Вы должны создать API для обработки CRUD запросов.
-    В данной задаче все пользователи хранятся в одном словаре, где ключ - это имя пользователя,
-    а значение - его параметры. {"user1": {"age": 33}, "user2": {"age": 20}}
-    Словарь (dict) хранить в памяти, он должен быть пустым при старте flask.
-
-    POST /user - создание пользователя.
-    В теле запроса приходит JSON в формате {"name": <имя пользователя>}.
-    Ответ должен вернуться так же в JSON в формате {"data": "User <имя пользователя> is created!"}
-    со статусом 201.
-    Если в теле запроса не было ключа "name", то в ответ возвращается JSON
-    {"errors": {"name": "This field is required"}} со статусом 422
-
-    GET /user/<name> - чтение пользователя
-    В ответе должен вернуться JSON {"data": "My name is <name>"}. Статус 200
-
-    PATCH /user/<name> - обновление пользователя
-    В теле запроса приходит JSON в формате {"name": <new_name>}.
-    В ответе должен вернуться JSON {"data": "My name is <new_name>"}. Статус 200
-
-    DELETE /user/<name> - удаление пользователя
-    В ответ должен вернуться статус 204
-    """
+    users = {}
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+        app.response_class = CustomResponse
+
+        @app.route('/user', methods=['POST'])
+        def create_user():
+            FlaskExercise.users.clear()
+            data = request.get_json()
+            if not data or 'name' not in data:
+                response = jsonify({'errors': {'name': 'This field is required'}})
+                response.status = 422
+                return response
+            name = data['name']
+            FlaskExercise.users[name] = {}
+            return jsonify({'data': f'User {name} is created!'}), 201
+
+        @app.route('/user/<name>', methods=['GET'])
+        def read_user(name):
+            if name in FlaskExercise.users:
+                return jsonify({'data': f'My name is {name}'})
+            else:
+                return jsonify({'errors': 'User not found'}), 404
+
+        @app.route('/user/<name>', methods=['PATCH'])
+        def update_user(name):
+            if name not in FlaskExercise.users:
+                return jsonify({'errors': 'User not found'}), 404
+            data = request.get_json()
+            if not data or 'name' not in data:
+                return jsonify({'errors': {'name': 'This field is required'}}), 422
+            new_name = data['name']
+            FlaskExercise.users[new_name] = FlaskExercise.users.pop(name)
+            return jsonify({'data': f'My name is {new_name}'}), 200
+
+        @app.route('/user/<name>', methods=['DELETE'])
+        def delete_user(name):
+            if name in FlaskExercise.users:
+                del FlaskExercise.users[name]
+                return '', 204
+            else:
+                return jsonify({'errors': 'User not found'}), 404
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    FlaskExercise.configure_routes(app)
+    app.run(debug=True)
